aabf29708992a0a68504c62a99362e9ea80aac65
smpeyres <smpeyres@gmail.com>
2024-09-24 12:47:06 -0500

    working on Boris-Bunemann, not finished, just saving progress

--- particle/python/boris.py
+++ particle/python/boris.py
@@ -5,56 +5,77 @@ import matplotlib.pyplot as plt
 sys.path.insert(1, '../../ode/python/')
 import ode
 
-qe = 1.60217662e-19
-me = 9.10938356e-31
-mp = 1.6726219e-27
+# physical constants
+qe = 1.60217662e-19 # C
+me = 9.10938356e-31 # kg
+mp = 1.6726219e-27 # kg
 
-# okay, I really need to get GitHub CoPilot... but it is not intelligent
-# then why would I get it? Maybe these easy small changes
-
-dt = 1.0e-9
-q = qe
-m = me
-params = np.array(( dt, q, m ))
-
+# define static, uniform E-field
 def Efield(x,y,z):
   Ex = 0.0
   Ey = 0.0
   Ez = 0.0
   return Ex, Ey, Ez
 
+# define static, uniform B-field
 def Bfield(x,y,z):
   Bx = 0.0
   By = 0.0
   Bz = 0.1
   return Bx, By, Bz
 
+# if user wants frequency correction, calculate and return it
+# see slide 17 of lesson 06
+# generic input for x, but should be Omega(x_n)*dt/2
 def frequency_correction(x, apply_correction=True):
-  if apply_correction:
+  if x == 0.0:
+    alpha = 1.0
+  elif apply_correction:
     alpha = np.tan(x)/x
   else:
     alpha = 1.0
   return alpha
 
+# Boris-Bunemann algorithm (Hockney-Bunemann at the very least)
+# takes time array, initial values array, and parameter array
 def boris_bunemann( time, X0, params ):
-  # time = np.linspace( 0.0, 1.0e-6, 100 )
-  dt = params[0]
-  q = params[1]
-  m = params[2]
-  apply_correction = params[3]
+  # calculate timestep (assumes constant timestep)
+  dt = time[1] - time[0]
 
-
-  qmdt2 = q * dt / (2.0 * m)
+  # get value of charge, mass, and if frequency correction on or off
+  q = params[0]
+  m = params[1]
+  apply_correction = params[2]
 
   # number of time steps
-  N = np.size(time) # why not np.len?
+  N = np.size(time)
 
   # state vector at every time step
+  # assumes 3D cartestian for 6D phase space
   X = np.zeros((N,6))
+
+  # setting the half-step back for velocities
+  Ex0, Ey0, Ez0 = Efield(X0[0],X0[1],X0[2])
+  Bx0, By0, Bz0 = Bfield(X0[0],X0[1],X0[2])
+
+  # 
+
 
   # fill the first row with ICs
   X[0,:] = X0
+  # x, y, z, vx, vy, vz
 
+
+  Ex0, Ey0, Ez0 = Efield(x,y,z)
+  Bx0, By0, Bz0 = Bfield(x,y,z)
+
+  vxm12 = X0[3] - 
+
+
+  for n in range(0, N-1):
+    
+
+  # determine the first 
   x  = X[0,0]
   y  = X[0,1]
   z  = X[0,2]
@@ -62,9 +83,10 @@ def boris_bunemann( time, X0, params ):
   vy = X[0,4]
   vz = X[0,5]
 
-  for n in range(0,N-1): # why not (0,N)?
-    Ex, Ey, Ez = EField(x,y,z)
-    Bx, By, Bz = BField(x,y,z)
+  # we made the rest of this in class, so I'll trust it for now
+  for n in range(0,N-1):
+    Ex, Ey, Ez = Efield(x,y,z)
+    Bx, By, Bz = Bfield(x,y,z)
 
     # Frequency correction factor alpha
     alpha_x = frequency_correction( qmdt2*Bx, apply_correction )
@@ -84,9 +106,9 @@ def boris_bunemann( time, X0, params ):
     ty = qmdt2 * By * alpha_y
     tz = qmdt2 * Bz * alpha_z
 
-    vprime_x += vy*tz - vz*ty
-    vprime_y += vz*tx - vx*tz
-    vprime_z += vx*ty - vy*tx
+    vprime_x = vy*tz - vz*ty
+    vprime_y = vz*tx - vx*tz
+    vprime_z = vx*ty - vy*tx
 
     # Step 2B. B-Field rotation
     tsq = tx*tx + ty*ty + tz*tz
@@ -97,7 +119,7 @@ def boris_bunemann( time, X0, params ):
 
     vpx = vx + vprime_y*sz - vprime_z*sy
     vpy = vy + vprime_z*sx - vprime_x*sz
-    vpz = vz + vprime_x*sy - vrpime_y*sx
+    vpz = vz + vprime_x*sy - vprime_y*sx
 
     # Step 3. Half acceleration (E-field)
     vx = vpx + qmdt2 * Ex * alpha_x
@@ -119,51 +141,45 @@ def boris_bunemann( time, X0, params ):
 
   return X
 
-# def fun(t,X):
-#   x, y, z, vx, vy, vz = X
-#   # Charge-to-mass ratio (q/m)
-#   qm = qe/me
-#   # E-field [V/m]
-#   Ex = 0.0
-#   Ey = 100.0
-#   Ez = 0.0
-#   # B-field [T]
-#   Bx = 0.0
-#   By = 0.0
-#   Bz = 1.0e-4
-#   # Newton-Lorentz equation in Cartesian coordinates
-#   Xdot = np.zeros(6)
-#   Xdot[0] = vx
-#   Xdot[1] = vy
-#   Xdot[2] = vz
-#   Xdot[3] = qm * ( Ex + vy*Bz - vz*By )
-#   Xdot[4] = qm * ( Ey + vz*Bx - vx*Bz )
-#   Xdot[5] = qm * ( Ez + vx*By - vy*Bx )
-#   return Xdot
-
+# numerical & physics parameters and no 
+dt = 1.0e-9
+q = qe
+m = me
+params = np.array(( q, m, False ))
 
 def main():
-  # Grid
-  time = np.linspace( 0.0, 1.1e-6, 100 )
-  # Initial conditions
-  X0 = np.array(( 0.0, 0.0, 0.0, 0.0, 1.0e6, 0.0 ))
-  # Solve ODE
-  X_ef = ode.euler( fun, time, X0 )       # Forward Euler
-  X_mp = ode.midpoint( fun, time, X0 )    # Explicit Midpoint
-  X_rk = ode.rk4( fun, time, X0 )         # Runge-Kutta 4
+  # physics parameters and freq correction
+  q = qe
+  m = me
+  params_nocorr = np.array(( q, m, False ))
+  params_corr = np.array(( q, m, True ))
 
-  # for i in range(0,xn.size):
-  #     print xn[i], y_an[i], y_ef[i,0], y_mp[i,0], y_rk[i,0]
+  cyclotron_freq = q*0.1/m # assuming uniform field of 0.1 T
+  period = 2*np.pi/cyclotron_freq
+
+  # 2 periods, 15 steps per period
+  time_2per = np.linspace(0, 2*period, 2*15)
+
+  # 100 periods, 15 steps per period
+  time_100per = np.linspace(0, 100*period, 100*15)
+
+  # Initial conditions - start at origin and positive velocity along y-direction
+  X0 = np.array(( 0.0, 0.0, 0.0, 0.0, 1.0e6, 0.0 ))
+
+  # Solve ODE
+  X_2per_nocorr = boris_bunemann(time_2per, X0, params_nocorr)
+  X_2per_corr = boris_bunemann(time_2per, X0, params_corr)
+  X_100per_nocorr = boris_bunemann(time_100per, X0, params_nocorr)
+  X_100per_corr = boris_bunemann(time_100per, X0, params_corr)
 
   plt.figure(1)
-  plt.plot( X_ef[:,0], X_ef[:,1], 'ro-', label='Forward Euler (1st)' )
-  plt.plot( X_mp[:,0], X_mp[:,1], 'go-', label='Explicit Mid-Point (2nd)' )
-  plt.plot( X_rk[:,0], X_rk[:,1], 'bx-', label='Runge-Kutta (4th)' )
+  plt.plot( X_2per_nocorr[:,0], X_2per_nocorr[:,1], 'o--', color='r', label='BB w/o Freq. Corr.' )
+  plt.plot( X_2per_corr[:,0], X_2per_corr[:,1], 'o--', color='b', label='BB w/ Freq. Corr.' )
   plt.xlabel('x [m]')
   plt.ylabel('y [m]')
   plt.axis('equal')
-  plt.legend(loc=3)
-  plt.savefig('ex01_particle_ExB.png')
+  plt.legend()
+  plt.savefig('boris_01.png',dpi=600)
   plt.show()
 
 if __name__ == '__main__':
