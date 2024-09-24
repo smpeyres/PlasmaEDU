7c544e31fa927ab6d22fbed3131a934df73a2120
smpeyres <smpeyres@gmail.com>
2024-09-23 21:23:14 -0500

    Incorporated freq correction to leapfrog

--- particle/python/ex13_leapfrogA4.py
+++ particle/python/ex13_leapfrogA4.py
@@ -28,7 +28,7 @@ def fun(t,X):
 # then, v_-1/2 = v_0 - 1/2 a_0 dt
 # where a = - O^2 x or Xdot[1]
 
-def leapfrog(fun, x, y0):
+def leapfrog(fun, x, y0, correction=True):
   # following the basic structure of the functions in ode.py
   N = np.size(x) # get number of timesteps
   h = x[1] - x[0] # get size of timestep
@@ -41,15 +41,28 @@ def leapfrog(fun, x, y0):
   # I'll keep it as simple 1D problem for now
   xn0, vn0 = y0
   an0 = fun(0, [xn0, vn0])[1]
+  freq = np.sqrt(-1*an0/xn0)
   ynm12 = vn0 - 0.5*an0*h
   y[0,:] = np.array([xn0,ynm12])
 
-  for n in range(0, N-1):
-    # update velocity first:
-    y[n+1,1] = y[n,1] + h*fun( x[n], y[n,:])[1]
-    # update position next:
-    y[n+1,0] = y[n,0] + h*y[n+1,1]
+  if correction == False:
+    for n in range(0, N-1):
+      # update velocity first:
+      y[n+1,1] = y[n,1] + h*fun( x[n], y[n,:])[1]
+      # update position next:
+      y[n+1,0] = y[n,0] + h*y[n+1,1]
+  else:
+    freq_corr = np.sin(0.5*freq*h)/(0.5*freq*h)
+    for n in range(0, N-1):
+      # update velocity first:
+      y[n+1,1] = y[n,1] + h*(freq_corr**2)*fun( x[n], y[n,:])[1]
+      # update position next:
+      y[n+1,0] = y[n,0] + h*y[n+1,1]
+
+
   return y
+
+
 
 
 def main():
@@ -63,7 +76,16 @@ def main():
 
   X_rk4 = ode.rk4(fun, time, X0)
 
-  X_leap = leapfrog(fun, time, X0)
+  X_leap_no_corr = leapfrog(fun, time, X0, correction=False)
+
+  X_leap_corr = leapfrog(fun, time, X0, correction=True)
+
+  X_an = np.cos(time)
+
+  error_rk4 = ode.error_absolute(X_an, X_rk4[:,0])
+  error_leap_no_corr = ode.error_absolute(X_an, X_leap_no_corr[:,0])
+  error_leap_corr = ode.error_absolute(X_an, X_leap_corr[:,0])
+
 
   # okay, now let's think about the leapfrog...
   # x_n+1 = x_n + v_n+1/2 dt
@@ -79,16 +101,27 @@ def main():
   # seems like we have some type of euler scheme then... but not quite...
 
 
-
-
-
   plt.figure(1)
   plt.plot(time_an/(2*np.pi), np.cos(time_an), '-', color='k', label="Analytical Solution")
-  plt.plot(time/(2*np.pi), X_rk4[:,0], 'o', color='b', label="Runge-Kutta 4")
-  plt.plot(time/(2*np.pi), X_leap[:,0], 'o', color='r', label="Leapfrog w/o Freq. Corr.")
+  plt.plot(time/(2*np.pi), X_rk4[:,0], 'o--', color='b', label="Runge-Kutta 4")
+  plt.plot(time/(2*np.pi), X_leap_no_corr[:,0], 'o--', color='r', label="Leapfrog w/o Freq. Corr.")
+  plt.plot(time/(2*np.pi), X_leap_corr[:,0], 'o--', color='g', label="Leapfrog w/ Freq. Corr.")
   plt.xlabel('time, $t\Omega/2\pi$')
   plt.ylabel('position, $x$')
-  plt.legend()
+  plt.legend(loc=3)
+  plt.draw()
+  plt.savefig("ex13_verification.png",dpi=600)
+  plt.show()
+
+  plt.figure(2)
+  plt.plot(time/(2*np.pi), error_rk4, 'o--', color='b', label="Runge-Kutta 4")
+  plt.plot(time/(2*np.pi), error_leap_no_corr, 'o--', color='r', label="Leapfrog w/o Freq. Corr.")
+  plt.plot(time/(2*np.pi), error_leap_corr, 'o--', color='g', label="Leapfrog w/ Freq. Corr.")
+  plt.xlabel('time, $t\Omega/2\pi$')
+  plt.ylabel('absolute error')
+  plt.legend(loc=0)
+  plt.draw()
+  plt.savefig("ex13_error.png",dpi=600)
   plt.show()
 
 
